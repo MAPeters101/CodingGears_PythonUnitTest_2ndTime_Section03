7bbdbed9ec91cc01f8fd7c64f22773a260472f9a
Mark Peters <mapeters101@earthlink.net>
2025-04-08 01:13:14 -0400

    Work on lesson011 from 3:10 to 4:15.  Test files must start with test to be picked up when running all tests in directory.

---
+++ test_lesson009_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+import lesson009_calculator
+
+
+class TestCalculator(unittest.TestCase):
+    def test_add_functionality(self):
+        result = lesson009_calculator.calc_add(10,20)
+        self.assertEqual(result, 30)
+
+    def test_add_functionality_two_negative_numbers(self):
+        result = lesson009_calculator.calc_add(-10,-20)
+        self.assertEqual(result, -30)
+
+
