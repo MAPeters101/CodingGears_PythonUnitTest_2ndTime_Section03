1be0425e9d161005d0509b92e5869af2295f58d8
Mark Peters <mapeters101@earthlink.net>
2025-04-08 00:44:12 -0400

    Complete work on lesson010 from 5:30.

--- lesson010_test_calculator.py
+++ lesson010_test_calculator.py
@@ -7,3 +7,9 @@ class TestsCalculator(unittest.TestCase):
         calc1 = Calculator(10, 30)
         result = calc1.calc_add()
         self.assertEqual(result, 40)
+
+    def test_add_functionality_with_one_negative_number(self):
+        calc1 = Calculator(10, -30)
+        result = calc1.calc_add()
+        self.assertEqual(result, -20)
+
